fcfd8ea6ce2f9476880bbf3132a8ef9fcdb46d46
TenKaiMass <mass.meroaune@gmail.com>
2021-12-03 12:29:15 +0100

    amelioration

--- App.py
+++ App.py
@@ -23,15 +23,15 @@ def index():
             'titre': 'un autre titre random',
         }
     ]
-    book = json.dumps(book)
+
     return jsonify(book)
     # search_term = request.args.get(book)
     # return search_term
     # return render_template('layout.html', title='hello my app', book=book)
 
 
-@app.route('/api/books/id', methods=['GET'])
-def getBookByid():
+@app.route('/api/books/<int:id>', methods=['GET'])
+def getBookByid(id):
 
     book = [
         {
@@ -45,18 +45,18 @@ def getBookByid():
         }
     ]
 
-    livreAlea = random.randint(0, 1)
-    print(livreAlea)
-    if book[livreAlea]['id'] == 1:
-        #book = json.dumps(book)
-        return jsonify(book[livreAlea])
-    if book[livreAlea]['id'] == 2:
-        #book = json.dumps(book)
-        return jsonify(book[livreAlea])
+    # livreAlea = random.randint(0, 1)
+    if id == 1:
+        return jsonify(book[0])
+    if id == 2:
+        # book = json.dumps(book)
+        return jsonify(book[1])
+    else:
+        return render_template('erreurBook.html', title='Fail')
 
 
-@app.route('/api/books/titre', methods=['GET'])
-def getBookByTitle():
+@app.route('/api/books/<titre>', methods=['GET'])
+def getBookByTitle(titre):
 
     book = [
         {
@@ -70,17 +70,17 @@ def getBookByTitle():
         }
     ]
 
-    livreAlea = random.randint(0, 1)
-    print(livreAlea)
-    if book[livreAlea]['titre'] == 'un titre':
-        return jsonify(book[livreAlea])
-    if book[livreAlea]['titre'] == 'un autre titre random':
-        return jsonify(book[livreAlea])
+    if titre == 'un titre':
+        return jsonify(book[0])
+    if titre == 'un autre titre random':
+        return jsonify(book[1])
+    else:
+        return render_template('erreurBook.html', title='Fail')
 
 
 @app.route('/api/books/catalogue', methods=["POST", "GET"])
 def getBooks():
-    with open("books.json", "r") as read_file:
+    with open("./books.json", "r") as read_file:
         data = json.load(read_file)
     # aleaBook = random.randint(0, len(data))
     # data = json.dumps(data[aleaBook])
@@ -88,8 +88,7 @@ def getBooks():
         livre = request.form["idortitle"]
         for i in range(0, len(data)):
             if data[i]['isbn'] == livre or data[i]['title'] == livre:
-                data = json.dumps(data[i])
-                return jsonify(data)
+                return render_template('search.html', title='searchRes', data=data[i])
             else:
                 return render_template('erreurBook.html', title='Fail')
 
@@ -99,4 +98,21 @@ def getBooks():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
+
+# from typing import Optional
+
+# from fastapi import FastAPI
+# import uvicorn
+
+# app = FastAPI()
+
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,13 @@
 # For more information, please refer to https://aka.ms/vscode-docker-python
-FROM python:slim
+FROM python:3.8
 
-EXPOSE 5000
-
-# Keeps Python from generating .pyc files in the container
-ENV PYTHONDONTWRITEBYTECODE=1
-
-# Turns off buffering for easier container logging
-ENV PYTHONUNBUFFERED=1
-
+ADD / .
 # Install pip requirements
 COPY requirements.txt .
 RUN python -m pip install -r requirements.txt
 
-WORKDIR /app
-COPY . /app
-
-# Creates a non-root user with an explicit UID and adds permission to access the /app folder
-# For more info, please refer to https://aka.ms/vscode-docker-python-configure-containers
-RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /app
-USER appuser
+#WORKDIR /app
+#COPY . /app
 
 # During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
-CMD ["gunicorn", "--bind", "http://127.0.0.1:5000", "App:app"]
+CMD ["python3","./App.py"]
